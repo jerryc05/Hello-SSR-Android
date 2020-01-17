8e57dab451af779a996c405130dd1b456b734e47
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-01-17 23:48:49 +0800

    minor tweak

--- app/src/main/java/io/jerryc05/hello_ssr/job/AclSyncJob.kt
+++ app/src/main/java/io/jerryc05/hello_ssr/job/AclSyncJob.kt
@@ -1,63 +1,55 @@
 package io.jerryc05.hello_ssr.job
 
-import io.jerryc05.hello_ssr.*
-import io.jerryc05.hello_ssr.utils.*
 import com.evernote.android.job.*
-import java.io.*
-import java.net.*
 import java.util.concurrent.*
 
-class AclSyncJob(private val route: String) : Job()
-{
-	override fun onRunJob(params: Params): Result
-	{
-		val filename = "$route.acl"
-		var inputStream: InputStream? = null
-		try
-		{
-			if ("self" == route)
-			{
-
-				inputStream = URL("https://raw.githubusercontent.com/HMBSbige/Text_Translation/master/ShadowsocksR/$filename").openConnection()
-					.getInputStream()
-				IOUtils.writeString(ShadowsocksApplication.app.applicationInfo.dataDir + '/'.toString() + filename, IOUtils.readString(inputStream!!))
-			}
-			return Result.SUCCESS
-		}
-		catch (e: IOException)
-		{
-			VayLog.e(TAG, "onRunJob", e)
-			ShadowsocksApplication.app.track(e)
-			return Result.RESCHEDULE
-		}
-		catch (e: Exception)
-		{
-			// unknown failures, probably shouldn't retry
-			VayLog.e(TAG, "onRunJob", e)
-			ShadowsocksApplication.app.track(e)
-			return Result.FAILURE
-		}
-		finally
-		{
-			try
-			{
-				inputStream?.close()
-			}
-			catch (e: IOException)
-			{
-				VayLog.e(TAG, "onRunJob", e)
-				ShadowsocksApplication.app.track(e)
-			}
-
-		}
+class AclSyncJob(private val route: String) : Job() {
+	override fun onRunJob(params: Params): Result { //todo
+		//		val filename = "$route.acl"
+		//		var inputStream: InputStream? = null
+		//		try
+		//		{
+		//			if ("self" == route)
+		//			{
+		//				inputStream = URL("https://raw.githubusercontent.com/HMBSbige/Text_Translation/master/ShadowsocksR/$filename").openConnection()
+		//					.getInputStream()
+		//				IOUtils.writeString(ShadowsocksApplication.app.applicationInfo.dataDir + '/'.toString() + filename, IOUtils.readString(inputStream!!))
+		//			}
+		//			return Result.SUCCESS
+		//		}
+		//		catch (e: IOException)
+		//		{
+		//			VayLog.e(TAG, "onRunJob", e)
+		//			ShadowsocksApplication.app.track(e)
+		//			return Result.RESCHEDULE
+		//		}
+		//		catch (e: Exception)
+		//		{
+		//			// unknown failures, probably shouldn't retry
+		//			VayLog.e(TAG, "onRunJob", e)
+		//			ShadowsocksApplication.app.track(e)
+		//			return Result.FAILURE
+		//		}
+		//		finally
+		//		{
+		//			try
+		//			{
+		//				inputStream?.close()
+		//			}
+		//			catch (e: IOException)
+		//			{
+		//				VayLog.e(TAG, "onRunJob", e)
+		//				ShadowsocksApplication.app.track(e)
+		//			}
+		//
+		//		}
+		return Result.FAILURE
 	}
 
-	companion object
-	{
+	companion object {
 		const val TAG = "AclSyncJob"
 
-		fun schedule(route: String): Int
-		{
+		fun schedule(route: String): Int {
 			return JobRequest.Builder("$TAG:$route")
 				.setExecutionWindow(1, TimeUnit.DAYS.toMillis(28))
 				.setRequirementsEnforced(true)
